aa782ef017cea13d1ac81fa2eef30dedbd3b7657
Michael J. Winton <wintonmj@me.com>
2017-03-08 19:50:20 +1300

    Added new AucklandRoadMap class

---
+++ src/main/java/AucklandRoadMap.java
@@ -0,0 +1,15 @@
+package main.java;
+
+/**
+ * Created by wintonnmj on 8/03/17.
+ */
+public class AucklandRoadMap {
+
+    public static void main(String[] args) {
+        new AucklandRoadMap();
+    }
+
+    public AucklandRoadMap() {
+
+    }
+}
